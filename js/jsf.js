9eca66e01375a82f29f0b5cf0c5f1e30a6e67e79
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2024-12-23 20:19:55 +0200

    Update jsf.js

--- js/jsf.js
+++ js/jsf.js
@@ -1,11 +1,15 @@
 document.querySelectorAll(".carousel-item").forEach((item) => {
-  item.addEventListener("click", function () {
+  const showModal = function () {
     const modalTitle = this.getAttribute("data-title");
     const modalBody = this.getAttribute("data-body");
     const modalImageSrc = this.querySelector("img").getAttribute("src");
 
     document.getElementById("exampleModalLabel").textContent = modalTitle;
-    document.querySelector(".modal-body p").textContent = modalBody;
     document.querySelector(".modal-body img").setAttribute("src", modalImageSrc);
-  });
+    document.querySelector(".modal-body p").textContent = modalBody;
+  };
+
+  // Add event listeners for both click and touchend
+  item.addEventListener("click", showModal);
+  item.addEventListener("touchend", showModal);
 });
